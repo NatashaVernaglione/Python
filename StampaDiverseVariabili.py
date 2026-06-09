6b2193a177ac2a7f6926b29fc8812a41c56c406e
NatashaVernaglione <vernaglionenatasha88@gmail.com>
2026-06-09 14:59:32 +0200

    Fix print statements for variable output

--- StampaDiverseVariabili.py
+++ StampaDiverseVariabili.py
@@ -5,5 +5,5 @@ intero2 = 18
 testo2 = "In Italia si diventa maggiorenni dopo i"
 
 
-print (testo + str(eta_int))
-print (testo2 + str(intero2))
+print (testo+' '+str(eta_int))
+print (testo2+' '+str(intero2))
